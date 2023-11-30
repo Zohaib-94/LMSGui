4b43ce94fb5b0b063f87ee4578b024b8f5f945a3
Zohaib-94 <143363565+Zohaib-94@users.noreply.github.com>
2023-11-29 23:19:31 -0500

    Update Book.java

--- Book.java
+++ Book.java
@@ -9,32 +9,65 @@ public class Book {
     private String author;
     private boolean available;
 
+
+    /**
+     * Constructor to initialize a Book object.
+     *
+     * @param barcode  The barcode of the book
+     * @param title    The title of the book
+     * @param author   The author of the book
+     */
     public Book(String barcode, String title, String author) {
         this.barcode = barcode;
         this.title = title;
         this.author = author;
         this.available = true;
     }
-
+    /**
+     * Returns barcode of the book.
+     *
+     * @return The barcode of the book
+     */
     public String getBarcode() {
         return barcode;
     }
-
+    /**
+     * Returns title of the book.
+     *
+     * @return The title of the book
+     */
     public String getTitle() {
         return title;
     }
-
+    /**
+     * Returns author of the book.
+     *
+     * @return The author of the book
+     */
     public String getAuthor() {
         return author;
     }
-
+    /**
+     * Checks if the book is available.
+     *
+     * @return True if the book is available; otherwise, false
+     */
     public boolean isAvailable() {
         return available;
     }
-
+    /**
+     * Sets the availability of the book.
+     *
+     * @param available The availability status of the book
+     */
     public void setAvailable(boolean available) {
         this.available = available;
     }
+    /**
+     * Generates a string representation of the Book object.
+     *
+     * @return A formatted string containing book details
+     */
 
     @Override
     public String toString() {
